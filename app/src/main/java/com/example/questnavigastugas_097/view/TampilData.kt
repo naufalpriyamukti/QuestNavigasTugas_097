0254929e9df032a70e76ed5acfefe385e298a081
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:38:53 +0700

    Tambah card untuk nama lengkap

--- app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
@@ -3,14 +3,22 @@ package com.example.questnavigastugas_097.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -32,6 +40,7 @@ fun TampilData(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -45,6 +54,22 @@ fun TampilData(
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Nama Lengkap
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 5.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+        ) {
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(stringResource(id= R.string.nama), fontWeight = FontWeight.Bold, fontSize = 14.sp)
+                Text(nama, fontStyle = FontStyle.Italic, fontSize = 16.sp, fontFamily = FontFamily.Cursive)
+            }
         }
 
 
