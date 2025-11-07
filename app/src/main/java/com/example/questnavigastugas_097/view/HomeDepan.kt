305660dc6634026189d66db0795d30bef36c7419
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:28:18 +0700

    Tambah spacer dan teks untuk nama dan nim

--- app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
@@ -46,5 +46,12 @@ fun HomeDepan(onSubmitClick: () -> Unit) {
             modifier = Modifier.size(180.dp)
         )
 
+        Spacer(modifier = Modifier.height(24.dp))
+        Text("Naufal Priyamukti Wibowo", color = Color.Black, fontSize = 18.sp)
+        Text("20250140001", color = Color.Gray, fontSize = 14.sp)
+
+
+
+
     }
 }
