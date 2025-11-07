871161b35f50bc9c39e873a4fdaae999ae0bcd49
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:26:09 +0700

    Tambah font family

--- app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
@@ -1,8 +1,40 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeDepan(onSubmitClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEDE7F6)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Text(
+            text = "Selamat Datang",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Monospace,
+            color = Color(0xFF512DA8)
+        )
 
+        Spacer(modifier = Modifier.height(24.dp))
+
+    }
 }
