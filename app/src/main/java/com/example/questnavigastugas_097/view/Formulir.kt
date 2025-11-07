7ab4cad2b904fdc26e313eb2e9bca2aa07d1e79b
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:10:26 +0700

    Tambah fungsi Formulir dan parameter onSubmitClick

--- app/src/main/java/com/example/questnavigastugas_097/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/Formulir.kt
@@ -1,2 +1,9 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Formulir(onSubmitClick: (String, String, String, String) -> Unit)
+{
+
+}
