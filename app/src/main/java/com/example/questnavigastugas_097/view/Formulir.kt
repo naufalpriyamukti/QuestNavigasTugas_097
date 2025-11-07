638a5eb2e162cd6d43ba548bb3211d134300ead3
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:11:15 +0700

    Tambah state dan daftar opsi untuk formulir

--- app/src/main/java/com/example/questnavigastugas_097/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/Formulir.kt
@@ -1,9 +1,22 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun Formulir(onSubmitClick: (String, String, String, String) -> Unit)
 {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+
+    val gender = listOf("Laki-laki", "Perempuan")
+    val statusList = listOf("Janda", "Lajang", "Duda")
+    val scrollState = rememberScrollState()
 
 }
