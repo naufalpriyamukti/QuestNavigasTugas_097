01d646fec694c6547e1004c23bc7cc4e7cc14c88
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:31:56 +0700

    Tambah TampilData dengan parameter data input

--- app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/TampilData.kt
@@ -1,2 +1,16 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+    nama: String,
+    jenis: String,
+    status: String,
+    alamat: String,
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit
+)
+{
+
+}
