c74e4b2000e49040739957489f57a0f695e4d219
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2025-11-07 21:27:18 +0700

    Tambah image untuk menampilkan gambar

--- app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
+++ app/src/main/java/com/example/questnavigastugas_097/view/HomeDepan.kt
@@ -1,20 +1,24 @@
 package com.example.questnavigastugas_097.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.questnavigastugas_097.R
 
 @Composable
 fun HomeDepan(onSubmitClick: () -> Unit) {
@@ -35,6 +39,12 @@ fun HomeDepan(onSubmitClick: () -> Unit) {
         )
 
         Spacer(modifier = Modifier.height(24.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.umy),
+            contentDescription = null,
+            modifier = Modifier.size(180.dp)
+        )
 
     }
 }
